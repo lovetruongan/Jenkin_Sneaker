12adb6d83bce9eb4892c2d2810eca43bc3646f8d
lovetruongan <lovetruongan@gmail.com>
2024-05-06 17:17:42 +0700

    change up

--- Sneakers-Web/src/environments/environment.development.ts
+++ Sneakers-Web/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8089/api/v1/',
-    apiImage: 'http://localhost:8089/api/v1/products/images/'
+    apiUrl: 'http://18.210.19.231:8089/api/v1/',
+    apiImage: 'http://18.210.19.231:8089/api/v1/products/images/'
 };
