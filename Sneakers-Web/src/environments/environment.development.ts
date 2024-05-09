defd1233fc7253eaa5ce6bd732313e00ac049862
lovetruongan <lovetruongan@gmail.com>
2024-05-09 14:03:27 +0700

    update elastic ip

--- Sneakers-Web/src/environments/environment.development.ts
+++ Sneakers-Web/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://18.210.19.231:8089/api/v1/',
-    apiImage: 'http://18.210.19.231:8089/api/v1/products/images/'
+    apiUrl: 'http://3.219.87.158:8089/api/v1/',
+    apiImage: 'http://3.219.87.158:8089/api/v1/products/images/'
 };
